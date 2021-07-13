1faca45ca5c88deed0c92b2c46bb6885f00c40e9
LuisLimaIt <pls.luis@gmail.com>
2021-07-13 01:22:46 -0300

    primeira aula Alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
+import { AluraKutMenu } from '../src/lib/AluraCommons';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSideBar(props) {
+  
+  return (
+    <Box>
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px'}}/>
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My Page</Title>
+  const githubUser = 'luislimait';
+  return (
+    <>
+      {/* <AluraKutMenu /> */}
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSideBar githubUser={githubUser}/>
+        </div>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>
+            Bem-Vindo
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: "profileRelationsArea" }}>
+          <Box>
+            Pessoas da Comunidade (Amigos(as))
+          </Box>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
